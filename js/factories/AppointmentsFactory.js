e130ab26b49ea160f2149397711406cd38a2d6e7
Maxine Ellah <maxine.ellah@gmail.com>
2016-08-02 21:52:56 +1200

    write appointmentFactory

---
+++ js/factories/AppointmentsFactory.js
@@ -0,0 +1,13 @@
+angular.module('ReminderApp', [])
+  .factory('AppointmentsFactory', function AppointmentsFactory($q, $http, $location) {
+    'use strict';
+    var exports = {};
+
+    exports.getMessages = function() {
+      return $http.get('json/appointments.json')
+      .error(function(data){
+        console.log('Theres and error!')
+      });
+    };
+    return exports;
+  })
