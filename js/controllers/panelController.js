41b2e2d305ca6d51fc773a7aefc173fe268f85d3
Maxine Ellah <maxine.ellah@gmail.com>
2016-08-03 20:14:43 +1200

    messages and failed panels on page

---
+++ js/controllers/panelController.js
@@ -0,0 +1,10 @@
+app.controller('PanelCtrl', function () {
+  this.tab = 1;
+
+  this.selectTab = function(setTab) {
+    this.tab = setTab;
+  };
+  this.isSelected = function(checkTab) {
+    return this.tab === checkTab;
+  };
+});
