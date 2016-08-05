da66a46647373f331156b991a97825888a6766b4
Maxine Ellah <maxine.ellah@gmail.com>
2016-08-05 18:30:36 +1200

    add display:flex to nav bar

--- js/app.js
+++ js/app.js
@@ -1,24 +1,6 @@
 var app = angular.module('ReminderApp', []);
-// (function() {
 
-
-  // app.controller('PanelController', function ($scope) {
-  //   $scope.tab = 1;
-  //
-  //   $scope.selectTab = function(setTab) {
-  //     $scope.tab = setTab;
-  //   };
-  //
-  //   $scope.isSelected = function(checkTab) {
-  //     return $scope.tab === checkTab;
-  //   };
-  //
-  // });
-// })();
-
-// angular.module('ReminderApp', [
-//   'ngRoute'
-// ]).config(function($routeProvider) {
+// app.config(function($routeProvider) {
 //   'use strict';
 //   //configure URLs
 //   $routeProvider
