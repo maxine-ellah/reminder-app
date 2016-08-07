86d8c4f6eb30ef44fe176627dfbe887c44bd210c
Maxine Ellah <maxine.ellah@gmail.com>
2016-08-08 11:51:19 +1200

    left-nav icons

--- js/directives/leftIcons.js
+++ js/directives/leftIcons.js
@@ -1,6 +1,6 @@
 app.directive('leftIcons', function(){
   return {
     restrict: 'E',
-    template: '<div class="left-icons"><p>icon 1</p><p>icon 2</p><p>icon 3</p><p>icon 4</p></div>'
+    template: '<div class="left-icons"><img src="images/mortar.png"><img src="images/house.png"><img src="images/pie.png"><img src="images/phone.png"></div>'
   };
 });
